8a3b19c10ae80e31e385ae1cbbd1e094dc33b6fb
ezermbu <ezermbu3@gmail.com>
2024-02-15 15:19:36 +0100

    package manager 1

---
+++ package_manager_1.py
@@ -0,0 +1,11 @@
+def install_or_remove_packages():
+    OrR = ""
+    while iOrR != "I" and iOrR != "R":
+        print("Vous souhaitez installer ou supprimer des packages? (I/R)")
+        iOrR = input().upper()
+        if iOrR == "I":
+            iOrR = "install"
+        elif iOrR == "R":
+            iOrR = "remove"
+
+install_or_remove_packages()
