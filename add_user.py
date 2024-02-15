4be6efa421032475e9a239211183ae7098001f53
ezermbu <ezermbu3@gmail.com>
2024-02-15 14:56:13 +0100

    create user script

---
+++ add_user.py
@@ -0,0 +1,11 @@
+import os
+
+def new_user():
+    confirm = "N"
+    while confirm != "Y":
+        username = input("Indiquez le nom de l'utilisateur à ajouter: ")
+        print("Utiliser username '" + username + "'? (Y/N)")
+        confirm = input().upper()
+        os.system("sudo adduser " + username)
+
+new_user()
